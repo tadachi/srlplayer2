681df3100011b00a752ab8132807742fa73088b1
Takumi Adachi <takumiadachi@gmail.com>
2014-10-14 09:39:39 -0700

    Beta. Very functional build. Finishing up and polishing.

--- app/js/srlplayer2.js
+++ app/js/srlplayer2.js
@@ -1,6 +1,6 @@
 /*
-* Similar to what you find in Java's format.
-* Usage: chatsrc = 'http://twitch.tv/chat/embed?channel={channel}&amp;popout_chat=true'.format({ channel: streamer});
+* Similar to what you find in Java"s format.
+* Usage: chatsrc = "http://twitch.tv/chat/embed?channel={channel}&amp;popout_chat=true".format({ channel: streamer});
 */
 if (!String.prototype.format) {
 	String.prototype.format = function() {
@@ -8,7 +8,7 @@ if (!String.prototype.format) {
 		if (!arguments.length)
 			return str;
 		var args = typeof arguments[0],
-			args = (('string' == args || 'number' == args) ? arguments : arguments[0]);
+			args = (("string" == args || "number" == args) ? arguments : arguments[0]);
 		for (arg in args)
 			str = str.replace(RegExp('\\{' + arg + '\\}', 'gi'), args[arg]);
 		return str;
@@ -19,26 +19,26 @@ if (!String.prototype.format) {
 * Parses the URL for querystring params.
 * Example usage:
 *   url = "http://dummy.com/?technology=jquery&blog=jquerybyexample".
-*   var tech = getQueryStringParams('technology'); //outputs: 'jquery'
-*   var blog = getQueryStringParams('blog');       //outputs: 'jquerybyexample'
+*   var tech = getQueryStringParams("technology"); //outputs: "jquery"
+*   var blog = getQueryStringParams("blog");       //outputs: "jquerybyexample"
 */
 function getQueryStringParams(sParam) {
 	var sPageURL      = window.location.href;
-	var sURLVariables = sPageURL.split('/');
+	var sURLVariables = sPageURL.split("/");
 	var querystring;
 	var count         = 0;
 	var key;
 	var value;
 
 	for (i = 0; i < sURLVariables.length; i++) {
-		if (sURLVariables[i].substring(0, 1) == '?') { // Found query string.
-			querystring = sURLVariables[i].substring(1).split('&');
+		if (sURLVariables[i].substring(0, 1) == "?") { // Found query string.
+			querystring = sURLVariables[i].substring(1).split("&");
 		}
 	}
 
 	if (querystring) {
 		for (i = 0; i < querystring.length; i++) {
-			arr = querystring[i].split('=');
+			arr = querystring[i].split("=");
 			key = arr[0];
 			value = arr[1];
 			if (key == sParam) { // key mataches param.
@@ -50,9 +50,19 @@ function getQueryStringParams(sParam) {
 	return null;
 }
 
-var app = angular.module('srlplayer2', []);
+Array.prototype.clean = function(deleteValue) {
+	for (var i = 0; i < this.length; i++) {
+		if (this[i] == deleteValue) {
+			this.splice(i, 1);
+				i--;
+			}
+		}
+	return this;
+};
 
-app.controller('MainController', function($scope, $http, $location) {
+var app = angular.module("srlplayer2", []);
+
+app.controller("MainController", function($scope, $http, $location) {
 	$scope.data0 = null;
 	$scope.data1 = null;
 	$scope.data2 = null;
@@ -65,16 +75,16 @@ app.controller('MainController', function($scope, $http, $location) {
 	$scope.category = "Speedruns" //Default starts on Speedruns
 
 	/* Populate stream selector tables */
-	$scope.categories = ['Speedruns', 'Starcraft', 'Hearthstone', 'Dota', 'Counterstrike', 'Hitbox', 'Azubu', 'Followed'];
+	$scope.categories = ["Speedruns", "Starcraft", "Hearthstone", "Dota", "Counterstrike", "Hitbox", "Azubu", "Followed"];
 	$scope.selection = $scope.categories[0]; // Default
 
 	$scope.urls = [ // List of REST api calls with all the streams we want.
-		'http://sa.tak.com:4001/speedruns?callback=JSON_CALLBACK',
-		'http://sa.tak.com:4001/starcraft?callback=JSON_CALLBACK',
-		'http://sa.tak.com:4001/hearthstone?callback=JSON_CALLBACK',
-		'http://sa.tak.com:4001/dota?callback=JSON_CALLBACK',
-		'http://sa.tak.com:4001/counterstrike?callback=JSON_CALLBACK',
-		'http://sa.tak.com:4001/hitbox?callback=JSON_CALLBACK'
+		"http://sa.tak.com:4001/speedruns?callback=JSON_CALLBACK",
+		"http://sa.tak.com:4001/starcraft?callback=JSON_CALLBACK",
+		"http://sa.tak.com:4001/hearthstone?callback=JSON_CALLBACK",
+		"http://sa.tak.com:4001/dota?callback=JSON_CALLBACK",
+		"http://sa.tak.com:4001/counterstrike?callback=JSON_CALLBACK",
+		"http://sa.tak.com:4001/hitbox?callback=JSON_CALLBACK"
 	];
 
 	$scope.refreshStreams = function() {
@@ -83,18 +93,15 @@ app.controller('MainController', function($scope, $http, $location) {
 		$http.get($scope.urls[2]).success(function(data) { $scope.data2 = data} );
 		$http.get($scope.urls[3]).success(function(data) { $scope.data3 = data} );
 		$http.get($scope.urls[4]).success(function(data) { $scope.data4 = data} );
-		$http.get($scope.urls[5]).success(function(data) { $scope.data5 = angular.fromJson(data); console.log(angular.fromJson(data)); } );
+		$http.get($scope.urls[5]).success(function(data) { $scope.data5 = angular.fromJson(data); console.log(angular.fromJson(data));} ); //console.log(angular.fromJson(data));
+		if (localStorage.getItem("twitch-username")) { loadStreams(function(data) { $scope.data7 = data.clean(null);  } ); }// console.log(data.clean(null));
 	}
 
-
 	function loadStreams(callback) {
-		twitchusername = ''; // Used for getting all streams that twitch user follows
-		if (localStorage.getItem('twitchusername')) {
-			twitchusername = localStorage.getItem('twitchusername')
-		}
+		twitch_username = localStorage.getItem("twitch-username"); // Used for getting all streams that twitch user follows
 
-		getAllFollowedStreams(twitchusername, function(results) {
-			callback(null, results);
+		getAllFollowedStreams(twitch_username, function(results) {
+			callback(results);
 		});
 	}
 
@@ -102,9 +109,9 @@ app.controller('MainController', function($scope, $http, $location) {
 		var offset     = 0; // get the next 25 people if you follow more than 25 people so increase offset by 25.
 		var limit      = 200;
 		$.ajax({
-			url: 'https://api.twitch.tv/kraken/users/{name}/follows/channels?direction =DESC&limit={limit}&offset={offset}&sortby=created_at'.format({ name: name, limit: limit, offset: offset }),
-			jsonp: 'callback',
-			dataType: 'jsonp', // tell jQuery we're expecting JSONP.
+			url: "https://api.twitch.tv/kraken/users/{name}/follows/channels?direction=DESC&limit={limit}&offset={offset}&sortby=created_at".format({ name: name, limit: limit, offset: offset }),
+			jsonp: "callback",
+			dataType: "jsonp", // tell jQuery we're expecting JSONP.
 
 			success: function(streams) {
 				if(streams.follows) {
@@ -141,11 +148,11 @@ app.controller('MainController', function($scope, $http, $location) {
 
 	function isStreamOnline(streamname, callback) {
 		$.ajax({
-			url      : 'https://api.twitch.tv/kraken/streams/' + streamname,
+			url      : "https://api.twitch.tv/kraken/streams/" + streamname,
 
-			jsonp    : 'callback',
+			jsonp    : "callback",
 
-			dataType : 'jsonp', // tell jQuery we're expecting JSONP.
+			dataType : "jsonp", // tell jQuery we're expecting JSONP.
 
 			success: function(stream) { // work with the response.
 				//console.log(stream);
@@ -163,10 +170,11 @@ app.controller('MainController', function($scope, $http, $location) {
 		center = calcCenter();
 
 		// Create the channel string based on chosen stream.
-		flashvars = 'channel={CHANNEL}&auto_play=true'.format({
+		flashvars = "channel={CHANNEL}&auto_play=true".format({
 			CHANNEL: streamername});
 
 		html =    ["<object bgcolor='#000000'",
+						"id='video'",
 						"data='http://www.twitch.tv/widgets/archive_embed_player.swf'",
 						"type='application/x-shockwave-flash'",
 						"width='{w}'".format({ w: center.w }),
@@ -177,9 +185,9 @@ app.controller('MainController', function($scope, $http, $location) {
 						"<param name='allowFullScreen' value='true' />",
 						"<param name='flashvars' value='{flashvars}'/>".format({flashvars: flashvars}),
 					"</object>"
-					].join('');
+					].join("");
 
-		//$("#center").html(html);
+		$("#center").html(html);
 	}
 
 	$scope.reloadTwitchChat = function(streamername) {
@@ -196,9 +204,9 @@ app.controller('MainController', function($scope, $http, $location) {
 						'src="{src}" '.format({src: src}),
 						'width="{w}" '.format({w: right.w}),
 						'height="{h}">'.format({h: right.h}),
-					'</iframe>'].join('');
+					'</iframe>'].join("");
 
-		//$('#right').html(html);
+		$("#right").html(html);
 	}
 
 	/* Reinjects HTML with the hitbox embed pointing to the stream. */
@@ -206,17 +214,18 @@ app.controller('MainController', function($scope, $http, $location) {
 		center = calcCenter();
 
 		// Create the channel string based on chosen stream.
-		flashvars = 'channel={CHANNEL}&auto_play=true'.format({
+		flashvars = "channel={CHANNEL}&auto_play=true".format({
 			CHANNEL: streamername});
 
 		html =    ["<iframe width='{w}' ".format({ w: center.w }),
 						"height='{h}' ".format({ h: center.h }),
+						"id='video'",
 						"src='http://hitbox.tv/#!/embed/{CHANNELNAME}?autoplay=true' ".format({ CHANNELNAME: streamername }),
 						"frameborder='0' ",
 						"allowfullscreen",
 						">",
 					"</iframe>"
-					].join('');
+					].join("");
 
 		$("#center").html(html);
 	}
@@ -225,35 +234,52 @@ app.controller('MainController', function($scope, $http, $location) {
 		right = calcRight();
 
 		html =  ["<iframe width='{w}' ".format({ w: right.w }),
+					"id='chat'",
 					"height='{h}' ".format({ h: right.h }),
 					"src='http://hitbox.tv/embedchat/{CHANNELNAME}' ".format({ CHANNELNAME: streamername }),
 					"frameborder='0' ",
 					"allowfullscreen",
 					">",
 				"</iframe>"
-				].join('');
+				].join("");
 
-		$('#right').html(html);
+		$("#right").html(html);
 	}
 
 	/* Events */
 	/* Catch ngRepeatFinished emitted event and parse for links in stream description and linkify it. */
 	$scope.$on("ngRepeatFinished", function(ngRepeatFinishedEvent) {
-		$('.description').linkify();
+		$(".description").linkify();
 	});
 
-	/* Main */
+	/* AngularJS Main */
 	$scope.refreshStreams();
+
+	/* On Refresh */
+	var streamer = getQueryStringParams("streamer");
+	var api = getQueryStringParams("api");
+
+	if(streamer) {
+		if (api == "twitch") {
+			$scope.reloadTwitchVideoPlayer(streamer);
+			$scope.reloadTwitchChat(streamer);
+		}
+		if (api == "hitbox") {
+			$scope.reloadHitboxVideoPlayer(streamer);
+			$scope.reloadHitboxChat(streamer);
+		}
+	}
+
 });
 
 // Emit an event when ngRepeat finishes rendering.
 app.directive("onFinishRender", function ($timeout) {
 	return {
-		restrict: 'A',
+		restrict: "A",
 		link: function (scope, element, attr) {
 			if (scope.$last === true) {
 				$timeout(function () {
-					scope.$emit('ngRepeatFinished');
+					scope.$emit("ngRepeatFinished");
 				});
 			}
 		}
@@ -295,7 +321,7 @@ function calcCenter() {
 function calcLeft() {
 	header = calcHeader();
 
-	width          = 307;
+	width          = 400;
 	height         = $(window).height() - header.h;
 
 	return {
@@ -321,26 +347,99 @@ function applyResolutions() {
 	right     = calcRight();
 	header    = calcHeader();
 
-	$('#container').css('width', container.w);
-	$('#container').css('height', container.h);
-	$('#header').css('width', header.w);
-	$('#header').css('height', header.h);
-	$('#center').css('width', container.w - left.w - right.w);
-	$('#center').css('height', center.h);
-	$('#right').css('width', right.w);
-	$('#right').css('height', right.h);
-	$('#left').css('width', left.w);
-	$('#left').css('height', left.h);
+	$("#container").css("width", container.w);
+	$("#container").css("height", container.h);
+	$("#header").css("width", header.w);
+	$("#header").css("height", header.h);
+	$("#center").css("width", container.w - left.w - right.w);
+	$("#center").css("height", center.h);
+	$("#right").css("width", right.w);
+	$("#right").css("height", right.h);
+	$("#left").css("width", left.w);
+	$("#left").css("height", left.h);
 
-	$('#settings-menu').css('width', left.w);
-	$('#settings-menu').css('height', 500);
-	$('#settings-menu').css('margin-top', header.h);
+	if (leftShow && rightShow) {
+		$('#right').css('width', right.w);
+		$('#right').css('height', right.h);
+		$('#left').css('width', left.w);
+		$('#left').css('height', left.h);
+	}
 
+	$("#settings-menu").css("width", left.w);
+	$("#settings-menu").css("height", 500);
+	$("#settings-menu").css("margin-top", header.h);
+
+	$('#video').css('width', container.w - left.w - right.w);
+	$('#video').css('height', center.h);
+	maximizeVideoPlayerResolution();
+
+	$('#chat').css('width', right.w);
+	$('#chat').css('height', right.h);
+}
+
+
+/* Resize */
+function maximizeVideoPlayerResolution() {
+	if (leftShow && !rightShow) { //Right is hidden so expand the videoplayer.
+		$('#left').css('width', left.w);
+		$('#left').css('height', left.h);
+		$('#center').css('width', (center.w + right.w));
+		$('#center').css('height', center.h);
+		$('#video').css('width', (center.w + right.w));
+		$('#video').css('height', center.h);
+	}
+	if (!leftShow && rightShow) { //Left is hidden so expand the videoplayer.
+		$('#right').css('width', right.w);
+		$('#right').css('height', right.h);
+		$('#center').css('width', (center.w + left.w));
+		$('#center').css('height', center.h);
+		$('#video').css('width', (center.w + left.w));
+		$('#video').css('height', center.h);
+	}
+	if ( (!leftShow && !rightShow) ) { //Both left and right are hidden so maximize videoplayer.
+		$('#center').css('width', (center.w + left.w + right.w));
+		$('#center').css('height', center.h);
+		$('#video').css('width', (center.w + left.w + right.w));
+		$('#video').css('height', center.h);
+	}
+}
+
+function toggleLeft() {
+	container = calcContainer();
+	center    = calcCenter();
+	left      = calcLeft();
+	right     = calcRight();
+
+	$('#left').toggle(); // Toggle show/hide
+	leftShow = !leftShow; // Toggle the boolean.
+
+	maximizeVideoPlayerResolution();
+
+	if (leftShow && rightShow) {
+		applyResolutions();
+	}
+}
+
+function toggleRight() {
+	container = calcContainer();
+	center    = calcCenter();
+	left      = calcLeft();
+	right     = calcRight();
+
+	$('#right').toggle();
+	rightShow = !rightShow;
+
+	maximizeVideoPlayerResolution();
+
+	if (leftShow && rightShow) {
+		applyResolutions();
+	}
 }
 
 var leftShow = true;
 var rightShow = true;
 
+/* JQuery Main */
 $( window ).resize(function() {
 	applyResolutions();
 });
@@ -348,8 +447,28 @@ $( window ).resize(function() {
 $( document ).ready(function() {
 	applyResolutions();
 
-	$('#settings').click(function() { //Show the settingsmenu div on click.
-		$('#settings-menu').toggle();
+	/*** Manage settings with session/localstorage ***/
+	$("#settings").click(function() { //Show the settingsmenu div on click.
+		$("#settings-menu").toggle();
 	})
 
+	if( localStorage.getItem("twitch-username") ) {
+		$("#twitch-username").val(localStorage.getItem("twitch-username"));
+		$("#show-followed-twitch-streams").prop("checked", true); // Checkbox is checked.
+		$("#twitch-username").prop("disabled",true); // Disable textbox.
+	}
+
+	$("#show-followed-twitch-streams").click(function() { // Toggle show followers on list of streamers.
+		if ($(this).is(":checked")) {
+			localStorage.setItem("twitch-username", $("#twitch-username").val());
+			$("#twitch-username").prop("disabled",true); // Disable textbox.
+		}else {
+			$("#twitch-username").prop("disabled",false); // Enable textbox.
+			localStorage.removeItem("twitch-username");
+		}
+	});
+
+	$("#apply").click(function() { //Refresh after applying settings.
+		location.reload();
+	});
 });
